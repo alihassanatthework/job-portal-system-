5dcfe3cf7b83187c0dde20762df297ab70757d28
alihassanatthework <alihassan.at.the.work@gmail.com>
2025-05-12 03:45:51 +0500

    did every thing normal  commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
-import { cartographer } from "@replit/vite-plugin-cartographer";
-import { shadcnThemeJson } from "@replit/vite-plugin-shadcn-theme-json";
 
 export default defineConfig({
   base: "/HandmadeCareers/",
   plugins: [
     react(),
-    cartographer(),
-    runtimeErrorOverlay(),
-    shadcnThemeJson(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-        ]
-      : []),
   ],
   resolve: {
     alias: {
